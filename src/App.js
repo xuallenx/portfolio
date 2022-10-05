866bfd78b7e178058ae32f1c8e600d11bff7b0ad
allenxu <allenx.03819@gmail.com>
2022-10-05 14:34:40 -0400

    Integrated React Router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Allen
-        
-        </a>
-      </header>
+   <div>
+      <h1>ALLEN</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        {/*java in curly brackets to distinguish from html in reach*/}
+        {/* below we wrap the text About with a link, the linke ends with ABOUT */}
+        <Link to="/about">About</Link> |{" "}  
+        <Link to="/projects">Project</Link>
+      </nav>
     </div>
   );
 }
---
+++ src/routes/projects.jsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Projects
+        </h2>
+      </main>
+    );
+  }
